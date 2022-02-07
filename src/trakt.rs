7f25f28586449b35094d0a2b7f410d78d2e45af3
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-02-07 17:20:20 +0000

    feat: add movie rating (#12)

--- src/trakt.rs
+++ src/trakt.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
-use ureq::Agent;
+use std::{collections::HashMap, time::Duration};
+use ureq::{Agent, AgentBuilder};
 
 #[derive(Serialize, Deserialize)]
 pub struct TraktMovie {
@@ -34,7 +35,7 @@ pub struct TraktIds {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct TraktBodyResponse {
+pub struct TraktWatchingResponse {
     pub expires_at: String,
     pub started_at: String,
     pub action: String,
@@ -44,25 +45,83 @@ pub struct TraktBodyResponse {
     pub episode: Option<TraktEpisode>,
 }
 
-pub fn get_watching(
-    agent: &Agent,
-    username: &String,
-    client_id: &String,
-) -> Option<TraktBodyResponse> {
-    let endpoint = format!("https://api.trakt.tv/users/{}/watching", username);
+#[derive(Serialize, Deserialize)]
+pub struct TraktRatingsResponse {
+    pub rating: f64,
+    pub votes: u32,
+    pub distribution: HashMap<String, u16>,
+}
 
-    let response: String = agent
-        .get(&endpoint)
-        .set("Content-Type", "application/json")
-        .set("trakt-api-version", "2")
-        .set("trakt-api-key", client_id)
-        .call()
-        .unwrap()
-        .into_string()
-        .unwrap();
+pub struct Trakt {
+    cache: HashMap<String, f64>,
+    agent: Agent,
+    client_id: String,
+    username: String,
+}
 
-    match serde_json::from_str(&response) {
-        Ok(response) => Some(response),
-        Err(_) => None,
+impl Trakt {
+    pub fn new(client_id: String, username: String) -> Trakt {
+        Trakt {
+            cache: HashMap::default(),
+            agent: AgentBuilder::new()
+                .timeout_read(Duration::from_secs(5))
+                .timeout_write(Duration::from_secs(5))
+                .build(),
+            client_id,
+            username,
+        }
+    }
+
+    pub fn get_watching(&self) -> Option<TraktWatchingResponse> {
+        let endpoint = format!("https://api.trakt.tv/users/{}/watching", self.username);
+
+        let response: String = self
+            .agent
+            .get(&endpoint)
+            .set("Content-Type", "application/json")
+            .set("trakt-api-version", "2")
+            .set("trakt-api-key", &self.client_id)
+            .call()
+            .unwrap()
+            .into_string()
+            .unwrap();
+
+        match serde_json::from_str(&response) {
+            Ok(response) => Some(response),
+            Err(_) => None,
+        }
+    }
+
+    pub fn get_movie_rating(&mut self, movie_slug: &String) -> Option<f64> {
+        match self.cache.get(movie_slug) {
+            Some(rating) => Some(*rating),
+            None => {
+                let endpoint = format!("https://api.trakt.tv/movies/{}/ratings", movie_slug);
+
+                let response: String = self
+                    .agent
+                    .get(&endpoint)
+                    .set("Content-Type", "application/json")
+                    .set("trakt-api-version", "2")
+                    .set("trakt-api-key", &self.client_id)
+                    .call()
+                    .unwrap()
+                    .into_string()
+                    .unwrap();
+
+                println!("{:?}", response);
+
+                match serde_json::from_str(&response) as Result<TraktRatingsResponse, _> {
+                    Ok(response) => {
+                        self.cache.insert(movie_slug.to_string(), response.rating);
+                        Some(response.rating)
+                    }
+                    Err(err) => {
+                        println!("{}", err);
+                        return None;
+                    }
+                }
+            }
+        }
     }
 }
