4054aea79fa187ef0e62d8d268cb926909fa9d2c
DareFox <easymalink@gmail.com>
2022-12-28 04:38:09 +0300

    main: provide tmdb client to Discord::set_activity

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@ use discrakt::{
     utils::{load_config, log},
 };
 use std::{thread::sleep, time::Duration};
+use tmdb::themoviedb::TMDb;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg = load_config();
     let mut discord = Discord::new(cfg.discord_token);
     let mut trakt = Trakt::new(cfg.trakt_client_id, cfg.trakt_username);
+    let tmdb_token: String = match cfg.tmdb_token {
+        // Burner account for out-of-the-box fetching
+        None => "835a79f3fecf7c9dfb6a767699ceac90".to_string(),
+        Some(token) => token
+    };
+
+    let tmdb_token_leaked: &'static str = Box::leak(tmdb_token.into_boxed_str());
     Discord::connect(&mut discord);
 
     loop {
@@ -24,6 +32,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         };
 
-        Discord::set_activity(&mut discord, &response, &mut trakt);
+
+        let tmdb = TMDb {
+            api_key: tmdb_token_leaked,
+            language: "en",
+        };
+        
+        Discord::set_activity(&mut discord, &response, &mut trakt, &tmdb);
     }
 }
