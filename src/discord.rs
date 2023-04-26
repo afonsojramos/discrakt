f7b64db2df42dc0014bf21939d98da9292cdc41d
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2023-04-26 19:45:39 +0100

    chore: improve code readability (#50)

--- src/discord.rs
+++ src/discord.rs
@@ -1,4 +1,3 @@
-use chrono::{DateTime, Utc};
 use discord_rich_presence::{
     activity::{Activity, Assets, Button, Timestamps},
     DiscordIpc, DiscordIpcClient,
@@ -7,11 +6,22 @@ use std::{thread::sleep, time::Duration};
 
 use crate::{
     trakt::{Trakt, TraktWatchingResponse},
-    utils::log,
+    utils::{get_watch_stats, log},
 };
 
 pub struct Discord {
     client: DiscordIpcClient,
+}
+
+#[derive(Default)]
+pub struct Payload {
+    pub details: String,
+    pub state: String,
+    pub media: String,
+    pub link_imdb: String,
+    pub link_trakt: String,
+    pub img_url: String,
+    pub watch_percentage: String,
 }
 
 impl Discord {
@@ -42,76 +52,70 @@ impl Discord {
         self.client.close().unwrap();
     }
 
-    pub fn set_activity(&mut self, trakt_response: &TraktWatchingResponse, trakt: &mut Trakt, tmdb_token: String) {
-        let details;
-        let state;
-        let media;
-        let link_imdb;
-        let link_trakt;
-        let id_tmdb;
-        let season_id ;
-        let img_url;
-        let start_date = DateTime::parse_from_rfc3339(&trakt_response.started_at).unwrap();
-        let end_date = DateTime::parse_from_rfc3339(&trakt_response.expires_at).unwrap();
-        let now = Utc::now();
-        let percentage = now.signed_duration_since(start_date).num_seconds() as f32
-            / end_date.signed_duration_since(start_date).num_seconds() as f32;
-        let watch_percentage = format!("{:.2}%", percentage * 100.0);
+    pub fn set_activity(
+        &mut self,
+        trakt_response: &TraktWatchingResponse,
+        trakt: &mut Trakt,
+        tmdb_token: String,
+    ) {
+        let mut payload_data = Payload::default();
 
-        match trakt_response.r#type.as_str() {
+        let img_url = match trakt_response.r#type.as_str() {
             "movie" => {
                 let movie = trakt_response.movie.as_ref().unwrap();
-                details = format!("{} ({})", movie.title, movie.year);
-                state = format!(
+                payload_data.details = format!("{} ({})", movie.title, movie.year);
+                payload_data.state = format!(
                     "{:.1} ⭐️",
                     Trakt::get_movie_rating(trakt, movie.ids.slug.as_ref().unwrap().to_string())
                 );
-                media = "movies";
-                link_imdb = format!(
+                payload_data.media = String::from("movies");
+                payload_data.link_imdb = format!(
                     "https://www.imdb.com/title/{}",
                     movie.ids.imdb.as_ref().unwrap()
                 );
-                link_trakt = format!(
+                payload_data.link_trakt = format!(
                     "https://trakt.tv/{}/{}",
-                    media,
+                    payload_data.media,
                     movie.ids.slug.as_ref().unwrap()
                 );
-                id_tmdb = movie.ids.tmdb.as_ref().unwrap();
-                img_url = trakt.get_movie_image(id_tmdb.to_string(), tmdb_token)
+                let id_tmdb = movie.ids.tmdb.as_ref().unwrap();
+                trakt.get_movie_image(id_tmdb.to_string(), tmdb_token)
             }
             "episode" if trakt_response.episode.is_some() => {
                 let episode = trakt_response.episode.as_ref().unwrap();
                 let show = trakt_response.show.as_ref().unwrap();
-                details = show.title.to_string();
-                state = format!("S{}E{} - {}", episode.season, episode.number, episode.title);
-                media = "shows";
-                link_imdb = format!(
+                payload_data.details = show.title.to_string();
+                payload_data.state =
+                    format!("S{}E{} - {}", episode.season, episode.number, episode.title);
+                payload_data.media = String::from("shows");
+                payload_data.link_imdb = format!(
                     "https://www.imdb.com/title/{}",
                     show.ids.imdb.as_ref().unwrap()
                 );
-                link_trakt = format!(
+                payload_data.link_trakt = format!(
                     "https://trakt.tv/{}/{}",
-                    media,
+                    payload_data.media,
                     show.ids.slug.as_ref().unwrap()
                 );
-                id_tmdb = show.ids.tmdb.as_ref().unwrap();
-                season_id = episode.season;
-                img_url = trakt.get_show_image(id_tmdb.to_string(), tmdb_token, season_id)
+                let id_tmdb = show.ids.tmdb.as_ref().unwrap();
+                trakt.get_show_image(id_tmdb.to_string(), tmdb_token, episode.season)
             }
             _ => {
                 log(&format!("Unknown media type: {}", trakt_response.r#type));
                 return;
             }
-        }
+        };
 
         let img = match img_url {
             Some(img) => img,
-            None => media.to_string(),
+            None => payload_data.media.to_string(),
         };
 
+        let watch_time = get_watch_stats(trakt_response);
+
         let payload = Activity::new()
-            .details(&details)
-            .state(&state)
+            .details(&payload_data.details)
+            .state(&payload_data.state)
             .assets(
                 Assets::new()
                     .large_image(&img)
@@ -120,14 +124,18 @@ impl Discord {
             )
             .timestamps(
                 Timestamps::new()
-                    .start(start_date.timestamp())
-                    .end(end_date.timestamp()),
+                    .start(watch_time.start_date.timestamp())
+                    .end(watch_time.end_date.timestamp()),
             )
             .buttons(vec![
-                Button::new("IMDB", &link_imdb),
-                Button::new("Trakt", &link_trakt),
+                Button::new("IMDB", &payload_data.link_imdb),
+                Button::new("Trakt", &payload_data.link_trakt),
             ]);
-        log(&format!("{details} - {state} | {watch_percentage}"));
+
+        log(&format!(
+            "{} - {} | {}",
+            payload_data.details, payload_data.state, watch_time.watch_percentage
+        ));
 
         if self.client.set_activity(payload).is_err() {
             self.connect();
--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,19 @@
-use chrono::{SecondsFormat, Utc};
+use chrono::{DateTime, FixedOffset, SecondsFormat, Utc};
 use configparser::ini::Ini;
+
+use crate::trakt::TraktWatchingResponse;
 
 pub struct Env {
     pub discord_token: String,
     pub trakt_username: String,
     pub trakt_client_id: String,
     pub tmdb_token: String,
+}
+
+pub struct WatchStats {
+    pub watch_percentage: String,
+    pub start_date: DateTime<FixedOffset>,
+    pub end_date: DateTime<FixedOffset>,
 }
 
 pub fn load_config() -> Env {
@@ -33,4 +41,18 @@ pub fn log(message: &str) {
         "{} : {message}",
         Utc::now().to_rfc3339_opts(SecondsFormat::Secs, true),
     );
+}
+
+pub fn get_watch_stats(trakt_response: &TraktWatchingResponse) -> WatchStats {
+    let start_date = DateTime::parse_from_rfc3339(&trakt_response.started_at).unwrap();
+    let end_date = DateTime::parse_from_rfc3339(&trakt_response.expires_at).unwrap();
+    let percentage = Utc::now().signed_duration_since(start_date).num_seconds() as f32
+        / end_date.signed_duration_since(start_date).num_seconds() as f32;
+    let watch_percentage = format!("{:.2}%", percentage * 100.0);
+
+    WatchStats {
+        watch_percentage,
+        start_date,
+        end_date,
+    }
 }
